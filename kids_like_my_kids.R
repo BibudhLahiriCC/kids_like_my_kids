3e8e5bb455277f553ad1375f00a60f541ff5eb43
Bibudh Lahiri <bibudh@casecommons.org>
2012-08-31 15:04:20 -0400

    Added function histograms_by_dimensions

--- kids_like_my_kids.R
+++ kids_like_my_kids.R
@@ -44,8 +44,14 @@ kids_like_my_kids <- function()
 
   dbDisconnect(con);
   #cat(paste(compute_distance(kid_metrics[1,], kid_metrics[2,]), "\n", sep = ""));
-  fn <- find_kNN(kid_metrics[1, ], kid_metrics[2:20001, ], 1000);
-  return(fn);
+  #N_values <- c(1000, 5000, 20000);
+  #k_values <- c(100, 2000, 5000);
+  #for (i in 1:3)
+  #{
+  #  fn <- find_kNN(kid_metrics[1, ], kid_metrics[2:(N_values[i] + 1), ], N_values[i], k_values[i]);
+  #}
+  #return(fn);
+  histograms_by_dimensions(kid_metrics);
 }
 
   categorize_age <- function(age_in_years)
@@ -124,7 +130,7 @@ compute_distance <- function(child_1, child_2)
    return(-1);
 }
 
- find_kNN <- function(my_kid, other_kids, k)
+ find_kNN <- function(my_kid, other_kids, N, k)
  {
    n_other_kids <- nrow(other_kids);
    for (i in 1:n_other_kids)
@@ -137,23 +143,27 @@ compute_distance <- function(child_1, child_2)
    cat(paste("LoS of most similar kids", "\n", sep = ""));
    print(cbind(other_kids[, "distance_with_my_kid"], other_kids[, "length_of_stay"]));
    cat("summary\n");
-   #edges <- c(0, 30, 60, 90, 120, 150, 180, 300, 400, 500, 600, 700, 8000);
+   descriptive_nums <- histogram_for_similar_kids(k_most_similar_kids, N, k); 
+   return(descriptive_nums);
+
+}
+
+  histogram_for_similar_kids <- function(k_most_similar_kids, N, k)
+  {
    descriptive_nums <- fivenum(k_most_similar_kids$length_of_stay);
-   filename <- paste("length_of_stay_histogram.png", sep = "");
+   filename <- paste("length_of_stay_histogram_", N, "_", k, ".png", sep = "");
    png(filename,  width = 920, height = 960, units = "px");
 
    histogram <- hist(k_most_similar_kids$length_of_stay, 
                         #breaks = edges, 
                         plot = FALSE);
    customHistogram(histogram = histogram, 
-         mainTitle = "Distribution of length of stay among kids like my kid",
+         mainTitle = "Distribution of LoS among kids like my kid",
          xLabel = "length in days", yLabel = "Fraction of children",
          descriptive_nums);
    dev.off();
-
    return(descriptive_nums);
-
-}
+  }
 
 customHistogram <- function(histogram, mainTitle, xLabel,
                             yLabel, fiveNumberSummary, 
@@ -183,19 +193,73 @@ customHistogram <- function(histogram, mainTitle, xLabel,
                           "-",
                           as.character(histogram$breaks[i+1]));
   }
-  subTitle <- paste("Q1 = ", round(fiveNumberSummary[2]),
+ 
+ subTitle <- paste("Q1 = ", round(fiveNumberSummary[2]),
                     ", Median = ", round(fiveNumberSummary[3]),
                     ", Q3 = ", round(fiveNumberSummary[4]),
                     sep = "");
   barplot(height = heights, width = widths, xlim = c(0, xAxisRightEnd),
           beside = TRUE, horiz = FALSE, main = mainTitle, xlab = xLabel,
-          ylab = yLabel, cex.lab = 0.75, space = 0, axisnames = TRUE, cex.names = 0.75,
-          cex.axis = 0.75, cex.main = 0.75, col.main= "blue",
+          ylab = yLabel, cex.lab = 1.5, space = 0, axisnames = TRUE, cex.names = 1.5,
+          cex.axis = 1.5, cex.main = 1.5, col.main= "blue",
           names.arg = barLabels, 
-          sub = subTitle, cex.sub = 0.75, col.sub = "red"
+          sub = subTitle, cex.sub = 1.5, col.sub = "red"
           );
 }
 
+  #Histograms to check what dimensions are important
+  histograms_by_dimensions <- function(kid_metrics)
+  {
+     covariates <- c("family_structure_single_female", "family_structure_single_male",
+                   "family_structure_married_couple",
+                   "family_structure_unmarried_couple",
+                   "parent_alcohol_abuse",
+                   "parent_drug_abuse",
+                   "child_behavioral_problem",
+                   "child_disability",
+                   "case_county_id",
+                   "assessment_county_id",
+                   "primary_caregiver_has_mental_health_problem",
+                   "domestic_violence_reported", 
+                   "count_previous_removal_episodes",
+                   "initial_placement_setting",
+                   "gender", "age_category",
+                   "count_previous_removal_episodes", "multi_racial", "american_indian", 
+                   "white", "black", "pacific_islander",
+                   "asian");
+      n_covariates <- length(covariates);
+      for (i in 1:n_covariates)
+      {
+          filename <- paste("./histograms_by_dimensions/length_of_stay_by_", covariates[i],".png", sep = "");
+          png(filename,  width = 920, height = 960, units = "px");
+          distinct_values <- unique(kid_metrics[, covariates[i]]);
+          distinct_values <- distinct_values[!is.na(distinct_values)];
+          n_distinct_values <- length(distinct_values);
+          par(mfrow=c(n_distinct_values, 1));
+          for (j in 1:n_distinct_values)
+          {
+            cat(paste("covariate = ", covariates[i], ", value = ", distinct_values[j], "\n", sep = ""));
+            kid_metrics_this_value <- subset(kid_metrics, (kid_metrics[, covariates[i]] == distinct_values[j]));
+            cat(paste("nrow = ", nrow(kid_metrics_this_value), ", ncol = ",
+                      ncol(kid_metrics_this_value), "\n", sep = ""));
+            histogram <- hist(kid_metrics_this_value$length_of_stay, 
+                                  plot = FALSE);
+            customHistogram(histogram = histogram, 
+                             mainTitle = paste("LoS for ", nrow(kid_metrics_this_value), 
+                                               " children with ", covariates[i], " = ", distinct_values[j], sep = ""),
+                             xLabel = "length in days", yLabel = "Fraction of children",
+                             fivenum(kid_metrics_this_value$length_of_stay));
+          }
+          dev.off();
+      }
+  }
+
+ 
+
+  #For a number K, take a random sample of K (child, removal episode) combinations as the test data.
+  #All the remaining data are training data. For each (child, removal episode) combination in the test data,
+  #take the k nearest neighbors from the test (child, removal episode) combinations, check the median LoS
+  #of the k NNs, and then find the difference of the median LoS with the LoS of the test kid. 
 
 
  
