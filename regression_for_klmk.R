28480a8ab9081ec4ff698b207ea934c37dcc548b
Bibudh Lahiri <bibudh@casecommons.org>
2012-09-14 10:04:15 -0400

    Added ridge_regression and stepwise_selection

--- regression_for_klmk.R
+++ regression_for_klmk.R
@@ -49,50 +49,115 @@ regression_for_klmk <- function()
   #cat(statement);
   res <- dbSendQuery(con, statement);
   kids_with_los <- fetch(res, n = -1);
-  print(kids_with_los[1:10, ]);
   kids_with_los$los_in_month <- kids_with_los$length_of_stay/30;
   kids_with_los$placements_per_month <- 
      kids_with_los$n_placements/kids_with_los$los_in_month;
-  create_linear_model(kids_with_los);
+
+  #Standardize the data and store the centers and scales
+  kids_with_los <- as.data.frame(scale(kids_with_los));
+  center_for_los <- attr(kids_with_los$length_of_stay, "scaled:center");
+  scale_for_los <- attr(kids_with_los$length_of_stay, "scaled:scale");
+  cat(paste("center_for_los = ", center_for_los, ", scale_for_los = ",
+            scale_for_los, "\n", sep = ""));
+
+  #correlation_matrix <- cor(kids_with_los);
+  #print(correlation_matrix);
+  cat(paste("base_error_rate = ", compute_base_error_rate(kids_with_los),
+             "\n", sep = ""));
+  linearModel <- create_linear_model(kids_with_los);
+  #ridge_regression(kids_with_los);
   dbDisconnect(con);
-  drops <- c("length_of_stay", "los_in_month", "n_placements");
+  drops <- c(
+             #"length_of_stay", 
+             "los_in_month", "n_placements");
   kids_with_los <- kids_with_los[,!(names(kids_with_los) %in% drops)];
-  print(colnames(kids_with_los));
-  subset_selection <- find_best_covariates(kids_with_los);
-  return(subset_selection);
+  #print(colnames(kids_with_los));
+  #stepwise_selection(kids_with_los);
+  #subset_selection <- find_best_covariates(kids_with_los);
+  return(linearModel);
+}
+
+#Compute the mean LoS, and see if it is used as the predicted value for
+#all kids and removal episodes, then how much the average prediction error 
+#would be
+compute_base_error_rate <- function(kids_with_los)
+{
+  cat(paste("With scaling, min LoS = ", min(kids_with_los$length_of_stay),
+            ", max LoS = ", max(kids_with_los$length_of_stay), 
+            ", median LoS = ", median(kids_with_los$length_of_stay),
+            "\n", sep = ""));
+  mean_LoS <- mean(kids_with_los$length_of_stay);
+  v <- rep(mean_LoS, nrow(kids_with_los));
+  base_error_rate <- mean((kids_with_los$length_of_stay - v)^2);
+  return(base_error_rate);
 }
 
 create_linear_model <- function(kids_with_los)
 {
-  linearModel <- lm(length_of_stay ~ 
-                     factor(age_category) 
-                      + factor(american_indian)
-                      + factor(asian)
-                      + factor(black)
-                      + factor(child_disability)
-                      + count_previous_removal_episodes
-                      + factor(family_structure_married_couple)
-                      + factor(parent_alcohol_abuse)
-                      + n_plcmnts_in_initial_months
-                      + placements_per_month
-                     , kids_with_los);
+  linearModel <- lm(length_of_stay ~  
+                      n_plcmnts_in_initial_months + factor(age_category) + 
+                          count_previous_removal_episodes + factor(black) 
+                          + factor(child_disability) + 
+                          factor(parent_alcohol_abuse),
+                          kids_with_los);
 
   print(summary(linearModel));
-
-  #Measure the relative error on each estimate and find median relative error
-  linearModel$observed_los <- linearModel$residuals + linearModel$fitted.values;
-  linearModel$relative_error <-  
-       abs(linearModel$residuals/linearModel$observed_los);
-  cat(paste("median relative error = ", 
-               round(median(linearModel$relative_error), 3), "\n", sep = ""));
+  if (FALSE)
+  {
+    #Measure the relative error on each estimate and find median relative error
+    linearModel$observed_los <- linearModel$residuals + linearModel$fitted.values;
+    linearModel$relative_error <-  
+         abs(linearModel$residuals/linearModel$observed_los);
+    cat(paste("median relative error = ", 
+                 round(median(linearModel$relative_error), 3), "\n", sep = ""));
+  }
+  cat(paste("average prediction error with this model = ", 
+               round(mean((linearModel$residuals)^2), 3), "\n", sep = ""));
+  return(linearModel);
 }
 
 
-#Find the best variables for each cardinality size, from a given set of cardinality sizes
+#Find the best variables for each cardinality size, from a given set of cardinality sizes,
+#using the Leaps and Bounds algorithm of Furnival and Wilson
 find_best_covariates <- function(kids_with_los)
 {
   library(subselect);
   correlation_matrix <- cor(kids_with_los);
   #print(eigen(correlation_matrix));
   eleaps(correlation_matrix,nsol=3, criterion="RM");
+}
+
+#Stepwise feature selection, using the Akaike Information Criterion.
+stepwise_selection <- function(kids_with_los)
+{
+  step(lm(length_of_stay ~1, kids_with_los), 
+       scope=list(lower=~1,
+                  upper = ~factor(age_category) 
+                          + factor(american_indian)
+                          + factor(asian)
+                          + factor(black)
+                          + factor(child_disability)
+                          + count_previous_removal_episodes
+                          + factor(family_structure_married_couple)
+                          + factor(parent_alcohol_abuse)
+                          + n_plcmnts_in_initial_months
+                          ));
+}
+
+ridge_regression <- function(kids_with_los)
+{
+  library(MASS);
+  ridgeModel <- lm.ridge(length_of_stay ~ factor(age_category) 
+                          + factor(american_indian)
+                          + factor(asian)
+                          + factor(black)
+                          + factor(child_disability)
+                          + count_previous_removal_episodes
+                          + factor(family_structure_married_couple) 
+                          + factor(family_structure_single_female) 
+                          + factor(parent_alcohol_abuse)
+                          + factor(parent_drug_abuse) 
+                          + n_plcmnts_in_initial_months
+                          + placements_per_month, kids_with_los);
+  print(summary(ridgeModel));
 }
