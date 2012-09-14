24d819aaf8ed3ec047ea3d07621979d502671e7a
Bibudh Lahiri <bibudh@casecommons.org>
2012-09-14 14:41:24 -0400

    Added linear_model_with_k_fold_cross_validation

--- regression_for_klmk.R
+++ regression_for_klmk.R
@@ -64,7 +64,9 @@ regression_for_klmk <- function()
   #print(correlation_matrix);
   cat(paste("base_error_rate = ", compute_base_error_rate(kids_with_los),
              "\n", sep = ""));
-  linearModel <- create_linear_model(kids_with_los);
+  linear_model_with_k_fold_cross_validation(kids_with_los, 10);
+
+  #linearModel <- create_linear_model(kids_with_los);
   #ridge_regression(kids_with_los);
   dbDisconnect(con);
   drops <- c(
@@ -74,7 +76,6 @@ regression_for_klmk <- function()
   #print(colnames(kids_with_los));
   #stepwise_selection(kids_with_los);
   #subset_selection <- find_best_covariates(kids_with_los);
-  return(linearModel);
 }
 
 #Compute the mean LoS, and see if it is used as the predicted value for
@@ -94,23 +95,32 @@ compute_base_error_rate <- function(kids_with_los)
 
 create_linear_model <- function(kids_with_los)
 {
-  linearModel <- lm(length_of_stay ~  
+  if (FALSE)
+  {
+    #The top 6 covariates that minimized the AIC most
+    linearModel <- lm(length_of_stay ~  
                       n_plcmnts_in_initial_months + factor(age_category) + 
                           count_previous_removal_episodes + factor(black) 
                           + factor(child_disability) + 
                           factor(parent_alcohol_abuse),
                           kids_with_los);
+  }
+  #Full model
+  linearModel <- lm(length_of_stay ~ factor(age_category) 
+                          + factor(american_indian)
+                          + factor(asian)
+                          + factor(black)
+                          + factor(child_disability)
+                          + count_previous_removal_episodes
+                          + factor(family_structure_married_couple) 
+                          + factor(family_structure_single_female) 
+                          + factor(parent_alcohol_abuse)
+                          + factor(parent_drug_abuse) 
+                          + n_plcmnts_in_initial_months
+                          , kids_with_los);
+
 
   print(summary(linearModel));
-  if (FALSE)
-  {
-    #Measure the relative error on each estimate and find median relative error
-    linearModel$observed_los <- linearModel$residuals + linearModel$fitted.values;
-    linearModel$relative_error <-  
-         abs(linearModel$residuals/linearModel$observed_los);
-    cat(paste("median relative error = ", 
-                 round(median(linearModel$relative_error), 3), "\n", sep = ""));
-  }
   cat(paste("average prediction error with this model = ", 
                round(mean((linearModel$residuals)^2), 3), "\n", sep = ""));
   return(linearModel);
@@ -160,4 +170,51 @@ ridge_regression <- function(kids_with_los)
                           + n_plcmnts_in_initial_months
                           + placements_per_month, kids_with_los);
   print(summary(ridgeModel));
+}
+
+linear_model_with_k_fold_cross_validation <- function(kids_with_los, k)
+{
+   kids_with_los$fold_id <- round(runif(nrow(kids_with_los), 1, k));
+   #We get one residual for each observation, because each observation is 
+   #used in the validation dataset exactly once
+   residual_matrix_start_point <- 1;
+   residuals <- data.frame(mat.or.vec(nrow(kids_with_los), 2));
+   colnames(residuals) <- c("scaled_los", "predicted_los");
+   
+
+   for (i in 1:k)
+   {
+     training_set <- subset(kids_with_los, (fold_id != i));
+     validation_set <- subset(kids_with_los, (fold_id == i));
+     cat(paste("k = ", k, ", i = ", i, 
+               ", size of training_set = ", nrow(training_set), 
+               ", size of validation_set = ", nrow(validation_set), "\n", sep = ""));
+     #The top 6 covariates that minimized the AIC most
+     linearModel <- lm(length_of_stay ~  
+                      n_plcmnts_in_initial_months + factor(age_category) + 
+                          count_previous_removal_episodes + factor(black) 
+                          + factor(child_disability) + 
+                          factor(parent_alcohol_abuse),
+                          training_set);
+     cat(paste("R-squared = ", (summary(linearModel))$r.squared, "\n", sep = ""));
+     predictions <- predict.lm(linearModel, validation_set);
+     cat(paste("filling in rows ",
+               residual_matrix_start_point,
+               " to ",
+               residual_matrix_start_point + nrow(validation_set)-1,
+               "\n", sep = ""));
+
+     l <- 1;
+     for (j in residual_matrix_start_point:
+          (residual_matrix_start_point + nrow(validation_set)-1))
+     {
+      residuals[j, "scaled_los"] <- validation_set[l, "length_of_stay"];
+      residuals[j, "predicted_los"] <- predictions[l];
+      l <- l + 1;
+     }
+     residual_matrix_start_point <- residual_matrix_start_point + 
+                                    nrow(validation_set);
+    }
+    cat(paste("cross-validated average prediction error with this model = ", 
+               round(mean((residuals$scaled_los - residuals$predicted_los)^2), 3), "\n", sep = ""));
 }
